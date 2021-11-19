4e45cda75f503d9076acf914f6f72d4f282e8c0f
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-11-19 18:37:06 +0300

    Create energyAndMiningSeries.py

---
+++ timeSeries/energyAndMiningSeries.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+pd.read_excel("/Users/rafiqkamal/Desktop/Data_Science/timeSeries/Energy and Mining 50 series 200 plus entities World Bank.xlsx")
+/Users/rafiqkamal/Desktop/Data_Science/timeSeries/Energy and Mining 50 series 200 plus entities World Bank.xlsx
